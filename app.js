c0d4e37af21058eeb098e2080a0d9070e976734d
JeongInPark <nickcaestro@naver.com>
2024-08-25 19:27:45 +0900

    App.js into heroku

--- app.js
+++ app.js
@@ -25,5 +25,5 @@ app.get('/barbershops', async (req, res) => {
 });
 
 // Listen on the specified port
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
