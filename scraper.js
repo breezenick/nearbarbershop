7e1f077070ca4e95f95d58a32b154316e9a9da01
JeongInPark <nickcaestro@naver.com>
2024-09-14 23:53:04 +0900

    Chrome 요청사항 수정

--- scraper.js
+++ scraper.js
@@ -1,18 +1,16 @@
 const puppeteer = require('puppeteer');
 
 async function scrapeInstagramImages(homePageUrl) {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    executablePath: '/usr/bin/google-chrome' // Make sure the correct path to Chrome is set
+  });
   const page = await browser.newPage();
-
   await page.goto(homePageUrl, { waitUntil: 'networkidle2' });
-
   const imageUrls = await page.evaluate(() => {
     return Array.from(document.querySelectorAll('img')).map(img => img.src);
   });
-
   await browser.close();
   return imageUrls;
 }
-
-
-module.exports = { scrapeInstagramImages };
