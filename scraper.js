f9c5589774c0527b9d3b2ff7e98b6a3f7928c3a7
JeongInPark <nickcaestro@naver.com>
2024-09-15 18:11:33 +0900

    필요없는 부분 삭제

--- scraper.js
+++ scraper.js
@@ -1,18 +1,20 @@
 const puppeteer = require('puppeteer');
 
-async function scrapeInstagramImages(homePageUrl) {
+async function scrapeInstagramPhotos(instagramUrl) {
   const browser = await puppeteer.launch({
     headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    executablePath: '/usr/bin/google-chrome' // Make sure the correct path to Chrome is set
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
   });
+
   const page = await browser.newPage();
-  await page.goto(homePageUrl, { waitUntil: 'networkidle2' });
+  await page.goto(instagramUrl, { waitUntil: 'domcontentloaded' });
+
   const imageUrls = await page.evaluate(() => {
     return Array.from(document.querySelectorAll('img')).map(img => img.src);
   });
+
   await browser.close();
   return imageUrls;
 }
 
-module.exports = { scrapeInstagramImages };
+module.exports = { scrapeInstagramPhotos };
