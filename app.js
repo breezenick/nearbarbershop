c945df0a55237789764b89af096e581ba2df78b4
JeongInPark <nickcaestro@naver.com>
2024-09-20 02:12:35 +0900

    app.js 수정
    barbershop 데이터 가져옴.

--- app.js
+++ app.js
@@ -18,5 +18,17 @@ app.get('/', (req, res) => {
   res.send('★★★★★ Welcome to the MongoDB API ★★★★★');
 });
 
+// Fetch all barbershops
+app.get('/barbershops', async (req, res) => {
+  try {
+    const results = await Barbershop.find({}); // Fetch all documents from the collection
+    res.json(results);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).send('Failed to retrieve data');
+  }
+});
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
